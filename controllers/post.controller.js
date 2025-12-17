8c4c5518d8330404c42e9ca86a3d924afbc74b3f
FAYRESU <664259005@webmail.npru.ac.th>
2025-12-17 22:38:23 +0700

    update Controller Post and Router Posts

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -64,7 +64,67 @@ exports.getAllPost = async (req, res) => {
 
 exports.getByID = async (req, res) => {
   const { id } = req.params;
+  try {
+    const posts = (await PostModel.findById(id).populate("author", ["username"]))
+      .sort({ createdAt: -1 })
+      .limit(20);
+    if (!posts) {
+      return res.status(404).send({ message: "Post not found" });
+    }
+    res.send(posts);
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || "Something error while retrieving the post",
+    });
+  }
 };
-exports.getByAuthorID = async (req, res) => {};
-exports.updatePost = async (req, res) => {};
-exports.deletePost = async (req, res) => {};
+
+exports.getByAuthorID = async (req, res) => { 
+  const { id } = req.params;
+  try {
+    const posts = (await PostModel.findById({author: id}).populate("author", ["username"]))
+      .sort({ createdAt: -1 })
+      .limit(20);
+    if (!posts) {
+      return res.status(404).send({ message: "Post not found" });
+    }
+    res.send(posts);
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || "Something error while retrieving the post",
+    });
+  }
+};
+exports.updatePost = async (req, res) => {try {
+    const { id } = req.params;
+    const posts = await PostModel.findById(id);
+    if (!posts)
+      return res.status(404).json({ message: "Post not found" });
+
+    if (req.body.name && req.body.name !== posts.name) {
+      const duplicate = await PostModel.findOne({
+        where: { name: req.body.name },
+      });
+      if (duplicate)
+        return res
+          .status(400)
+          .send({ message: "Posts is already existed" });
+    }
+
+    await posts.update(req.body);
+    res.status(200).json({ message: "Post updated successfully", posts });
+  } catch (error) {
+    res.status(500).json({ message: error.message || "Failed to update post" });
+  }
+};
+exports.deletePost = async (req, res) => { try {
+    const { id } = req.params;
+    const posts = await PostModel.findById(id);
+    if (!posts) return res.status(404).json({ error: "Post not found" });
+
+    await posts.destroy();
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message || "Failed to delete post" });
+  }
+};
